c493e20494debdf8fe7c66e4ace58722fe88d613
ax-lab <98651633+ax-lab@users.noreply.github.com>
2022-06-07 00:27:35 -0300

    feat: game layer & ctrl update
    - remove double press from ctrl key
    - update game layer mapping

--- layouts/main/layer_map.h
+++ layouts/main/layer_map.h
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[LAYER_MAX_COUNT][MATRIX_ROWS][MATRIX_COLS] = {
 		D_ESC   , KC_1    , KC_2    , KC_3    , KC_4    , KC_5    , MO_FN   ,       MO_FN   , KC_6    , KC_7    , KC_8    , KC_9    , KC_0    , TG_UTIL ,
 		KC_TAB  , KC_Q    , KC_W    , KC_E    , KC_R    , KC_T    , _______ ,       TG_PAD  , KC_Y    , KC_U    , KC_I    , KC_O    , KC_MINS , KC_EQUAL,
 		KC_BSPC , KC_A    , KC_S    , KC_D    , KC_F    , KC_G    , KC_DEL  ,       KC_ESC  , KC_H    , KC_J    , KC_K    , KC_L    , KC_P    , KC_SCLN ,
-		S_CTRL  , KC_Z    , KC_X    , KC_C    , KC_V    , KC_B    ,                           KC_N    , KC_M    , KC_COMMA, KC_DOT  , KC_QUOTE, KC_UP   ,
+		KC_LCTRL, KC_Z    , KC_X    , KC_C    , KC_V    , KC_B    ,                           KC_N    , KC_M    , KC_COMMA, KC_DOT  , KC_QUOTE, KC_UP   ,
 		KC_LALT , KC_LGUI , MO_FN   , MO_NAV_L, S_SHIFT ,           MO_ACC  ,       MO_ACC  ,           S_SYMBOL, MO_NAV_R, KC_LEFT , KC_RIGHT, KC_DOWN ,
 		                                        KC_ENT  , KC_TAB  , KC_SPC  ,       KC_ENT  , SFT_TAB , KC_SPC
 	),
@@ -45,17 +45,17 @@ const uint16_t PROGMEM keymaps[LAYER_MAX_COUNT][MATRIX_ROWS][MATRIX_COLS] = {
 	[LAYER_GAME] = LAYOUT_moonlander(
 		KC_ESC  , _______ , _______ , _______ , _______ , _______ , KC_PAUSE,       TO_BASE , _______ , _______ , _______ , _______ , _______ , _______ ,
 		KC_TAB  , _______ , _______ , _______ , _______ , _______ , _______ ,       _______ , _______ , _______ , _______ , _______ , _______ , _______ ,
-		KC_LCTRL, _______ , _______ , _______ , _______ , _______ , KC_DEL  ,       _______ , _______ , _______ , _______ , _______ , _______ , _______ ,
-		KC_LSFT , _______ , _______ , _______ , _______ , _______ ,                           _______ , _______ , _______ , _______ , _______ , _______ ,
-		KC_LALT , KC_LALT , KC_GRAVE, MO_GMOD , KC_SPC  ,           KC_ENT  ,       TO_GTYPE,           _______ , _______ , _______ , _______ , _______ ,
-		                                        KC_CAPS , KC_BSPC , KC_ESC  ,       _______ , _______ , _______
+		KC_LSFT , _______ , _______ , _______ , _______ , _______ , KC_DEL  ,       _______ , _______ , _______ , _______ , _______ , _______ , _______ ,
+		KC_LCTRL, _______ , _______ , _______ , _______ , _______ ,                           _______ , _______ , _______ , _______ , _______ , _______ ,
+		KC_LALT , KC_CAPS , KC_GRAVE, MO_GMOD , KC_SPC  ,           KC_ENT  ,       TO_GTYPE,           _______ , _______ , _______ , _______ , _______ ,
+		                                        KC_LSFT , KC_BSPC , KC_ESC  ,       _______ , _______ , _______
 	),
 	// Game mod:
 	[LAYER_G_MOD] = LAYOUT_moonlander(
 		_______ , KC_F10  , KC_F11  , KC_F12  , _______ , _______ , _______ ,       _______ , _______ , _______ , _______ , _______ , _______ , _______ ,
-		_______ , KC_F1   , KC_F2   , KC_F3   , _______ , _______ , _______ ,       _______ , _______ , KC_KP_7 , KC_KP_8 , KC_KP_9 , _______ , _______ ,
-		_______ , KC_F4   , KC_F5   , KC_F6   , _______ , _______ , _______ ,       _______ , _______ , KC_KP_4 , KC_KP_5 , KC_KP_6 , _______ , _______ ,
-		_______ , KC_F7   , KC_F8   , KC_F9   , _______ , _______ ,                           KC_NLCK , KC_KP_1 , KC_KP_2 , KC_KP_3 , _______ , _______ ,
+		_______ , KC_F1   , KC_F2   , KC_F3   , KC_Y    , KC_U    , _______ ,       _______ , _______ , KC_KP_7 , KC_KP_8 , KC_KP_9 , _______ , _______ ,
+		_______ , KC_F4   , KC_F5   , KC_F6   , KC_H    , KC_J    , _______ ,       _______ , _______ , KC_KP_4 , KC_KP_5 , KC_KP_6 , _______ , _______ ,
+		_______ , KC_F7   , KC_F8   , KC_F9   , KC_N    , KC_M    ,                           KC_NLCK , KC_KP_1 , KC_KP_2 , KC_KP_3 , _______ , _______ ,
 		_______ , _______ , _______ , _______ , _______ ,           _______ ,       _______ ,           KC_KP_0 , _______ , _______ , _______ , _______ ,
 		                                        _______ , _______ , _______ ,       _______ , _______ , _______
 	),
